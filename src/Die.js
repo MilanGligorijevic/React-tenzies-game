05c3ce85737b426610001bd22afde850f39d7bc3
Milan Gligorijevic <milannnngligorijevic@gmail.com>
2023-06-01 18:03:58 +0200

    Die Color update

--- src/Die.js
+++ src/Die.js
@@ -2,7 +2,14 @@ import React from "react";
 
 function Die(props){
 
+    let dieColor = "#F5F5F5";
+    
+    if(props.isHeld) {
+        dieColor = "#04AA6D";
+    }
+    
     const styles = {
+        backgroundColor: dieColor,
     }
 
     return (
